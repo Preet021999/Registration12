61a270bc8d3f6bf47909ad596f8b64d419625069
preet <preetmadhiwala1608@gmail.com>
2022-09-09 15:16:45 +0530

    Customer Home Page

--- app/src/main/java/com/example/registration/customer_home.java
+++ app/src/main/java/com/example/registration/customer_home.java
@@ -2,13 +2,44 @@ package com.example.registration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class customer_home extends AppCompatActivity {
+    Button btnop,btnest,btnlogout,btnbill;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_home);
+        btnest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(customer_home.this,estimatecost.class));
+                finish();
+            }
+        });
+        btnop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(customer_home.this,order_page.class));
+                finish();
+            }
+        });
+        btnlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(customer_home.this,LoginActivity.class));
+                finish();
+            }
+        });
+        btnbill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(customer_home.this,bill.class));
+            }
+        });
     }
 }
