5b2537e54922eaccd74fb2cd520b9b7a198893a9
preet <preetmadhiwala1608@gmail.com>
2022-09-09 07:42:18 +0530

    Payment

--- app/src/main/java/com/example/registration/bill.java
+++ app/src/main/java/com/example/registration/bill.java
@@ -2,12 +2,25 @@ package com.example.registration;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class bill extends AppCompatActivity {
+    Button billpay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bill);
+        billpay = findViewById(R.id.btpay);
+        String sAmount = "100000";
+
+        int amount = Math.round(Float.parseFloat(sAmount) * 100);
+        billpay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
