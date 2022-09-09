24c72629388775ad461cf776bbfc869b773385c4
preet <preetmadhiwala1608@gmail.com>
2022-09-09 16:54:19 +0530

    Estimate cost set

--- app/src/main/java/com/example/registration/bill.java
+++ app/src/main/java/com/example/registration/bill.java
@@ -1,11 +1,19 @@
 package com.example.registration;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class bill extends AppCompatActivity {
+import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class bill extends AppCompatActivity implements PaymentResultListener {
     Button billpay;
 
     @Override
@@ -19,8 +27,35 @@ public class bill extends AppCompatActivity {
         billpay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Checkout checkout = new Checkout();
+                checkout.setKeyID("rzp_test_sNummj8smufOdn");
+                checkout.setImage(com.razorpay.R.drawable.rzp_logo);
+                JSONObject object =new JSONObject();
+                try {
+                    object.put("name","Android coding");
+                    object.put("Description","Test Paymnet");
+                    object.put("theme.color","0093DD");
+                    object.put("Currency","INR");
+                    object.put("amount",amount);
+                    checkout.open(bill.this,object);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
             }
         });
+    }
+
+    @Override
+    public void onPaymentSuccess(String s) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Payment ID");
+        builder.setMessage(s);
+        builder.show();
+    }
+
+    @Override
+    public void onPaymentError(int i, String s) {
+        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_SHORT).show();
     }
 }
